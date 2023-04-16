c72a3be344ae1e2026b342065716e0e99a286512
omarbesi04 <omarbessi04@gmail.com>
2023-04-16 21:30:20 +0000

    new file:   Verkefni00-HalloHeimur/index.html 	new file:   Verkefni00-HalloHeimur/sketch.js 	new file:   Verkefni01-Mondrain/index.html 	new file:   Verkefni01-Mondrain/sketch.js 	new file:   index.html 	new file:   p5.js 	new file:   style.css

---
+++ Verkefni01-Mondrain/sketch.js
@@ -0,0 +1,11 @@
+// Hér kemur kóðinn þinn:
+
+function setup(){
+    createCanvas(300,300);
+    background(0,255,0);
+    fill(150);
+    ellipse(100,100,50,50);
+    fill(0);
+    textSize(20);
+    text("Halló heimur",200,40);
+}
